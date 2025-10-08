816c670f9ea8bc51dfb4a7fcb8d5b76034045b9e
AlexysAlves <alexys.alves0225@gmail.com>
2025-10-08 01:13:46 -0300

    test frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>AeroSynth Frontend 🚀</h1>
+      <p>Se tudo funcionou, você está vendo isso!</p>
+    </div>
+  );
+}
+
+export default App;
